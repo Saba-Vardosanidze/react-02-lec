7a762b46586433ebddfe969eca316e5401c184ba
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-03-13 21:59:06 +0400

    update

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+function App() {
+  const fruits = [
+    {
+      id: 1,
+      name: "Apple",
+      price: 2,
+      weight: 150,
+      img: "https://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg",
+    },
+    {
+      id: 2,
+      name: "Banana",
+      price: 1,
+      weight: 120,
+      img: "https://upload.wikimedia.org/wikipedia/commons/8/8a/Banana-Single.jpg",
+    },
+    {
+      id: 3,
+      name: "Cherry",
+      price: 3,
+      weight: 10,
+      img: "https://upload.wikimedia.org/wikipedia/commons/b/bb/Cherry_Stella444.jpg",
+    },
+    {
+      id: 4,
+      name: "Mango",
+      price: 4,
+      weight: 200,
+      img: "https://upload.wikimedia.org/wikipedia/commons/9/90/Hapus_Mango.jpg",
+    },
+    {
+      id: 5,
+      name: "Pineapple",
+      price: 5,
+      weight: 1200,
+      img: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Pineapple_and_cross_section.jpg",
+    },
+    {
+      id: 6,
+      name: "Orange",
+      price: 2,
+      weight: 180,
+      img: "https://upload.wikimedia.org/wikipedia/commons/c/c4/Orange-Fruit-Pieces.jpg",
+    },
+    {
+      id: 7,
+      name: "Strawberry",
+      price: 6,
+      weight: 30,
+      img: "https://upload.wikimedia.org/wikipedia/commons/2/29/PerfectStrawberry.jpg",
+    },
+  ];
+  // let toUpperCase = fruits.map((eachElement) => {
+  //   return eachElement.toUpperCase();
+  // });
+
+  // console.log(toUpperCase);
+
+  return <div className="gridContainer"></div>;
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,4 @@
+import Header from "./components/Header.css";
+export default function Header() {
+  return <header>hello world</header>;
+}
